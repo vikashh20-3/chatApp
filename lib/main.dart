7241596ed28d85f4ddfadec41b9caa47f9fa5818
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-26 20:15:06 +0530

    code updated

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:chatapp/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'We Chat ',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
+        appBarTheme:const  AppBarTheme(
+          elevation: 1,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+              color: Colors.black
+          ),
+          backgroundColor: Colors.white,
+        )
+      ),
+      home: const HomeScreen()
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: const Text("We Chat   "),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)
+          )
+        ],
+        leading: Icon(CupertinoIcons.home),
+      ),
+
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: FloatingActionButton(
+
+          onPressed: (){},
+          child: Icon(Icons.add_comment_rounded),
+        ),
+      ),
+    );
+  }
+}
