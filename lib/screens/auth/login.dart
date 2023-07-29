ef1f20453b3408c193bfc017945dc94d232a84be
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-29 21:47:24 +0530

    sign in done

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,10 +1,51 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
   State<Login> createState() => _LoginState();
+}
+
+@override
+// GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+// googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+// googleProvider.setCustomParameters({
+//   'login_hint': 'user@example.com'
+// });
+
+_googleBtnClick() {
+  _signInWithGoogle().then((user) {
+    log('\nUser : ${user.user}');
+    // Navigator.pushReplacement(,
+    //     MaterialPageRoute(builder: (context) => const HomeScreen()));
+  });
+}
+
+//GOOGLE SIGN IN CODE FOR WEB
+
+//GOOGLE SIGN IN CODE FOR ANDROID AND IOS
+Future<UserCredential> _signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
 }
 
 class _LoginState extends State<Login> {
@@ -26,19 +67,24 @@ class _LoginState extends State<Login> {
             width: MediaQuery.of(context).size.width * .5,
             height: MediaQuery.of(context).size.height * .30,
             left: MediaQuery.of(context).size.width * .25,
-            child: Image.asset(
-                '/home/vikash/Desktop/Flutter/chatapp/images/chat.png')),
+            child: Image.asset('images/chat.png')),
         Positioned(
-          width: MediaQuery.of(context).size.width * .75,
+          width: MediaQuery.of(context).size.width - 40,
           bottom: MediaQuery.of(context).size.height * .12,
           left: MediaQuery.of(context).size.width * .15,
           height: MediaQuery.of(context).size.width * .08,
           child: ElevatedButton.icon(
             style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.lightBlue, elevation: 9),
-            onPressed: () {},
+            onPressed: () {
+              _googleBtnClick();
+              // Navigator.push(
+              //     context,
+              //     MaterialPageRoute(
+              //         builder: ((context) => const HomeScreen())));
+            },
             icon: Image.asset(
-              '/home/vikash/Desktop/Flutter/chatapp/assets/google.png',
+              'images/google.png',
               height: MediaQuery.of(context).size.height * .05,
             ),
             label: RichText(
