bb9b6814774351950ba0473a6761231d50ead6a3
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-29 19:10:53 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'firebase_options.dart';
 import 'screens/auth/login.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   _initializeFirebase();
   runApp(const MyApp());
 }
